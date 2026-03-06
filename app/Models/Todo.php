fbef3b441cef550e80e62ac1fe0a414f73426fd9
Danyil <34955388-QuadrantSkillful@users.noreply.gitlab.com>
2026-03-06 16:32:08 +0000

    Update 2 files
    - /app/Models/Todo.php
    - /app/Models/User.php

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Todo extends Model
 {
-    //
+    // 1. Дозволяємо Laravel зберігати ці поля напряму (захист від вразливостей)
+    protected $fillable = [
+        'title',
+        'description',
+        'done',
+        'user_id',
+    ];
+
+    // 2. Вказуємо тип колонки 'done', щоб Laravel автоматично перетворював її на true/false
+    protected $casts = [
+        'done' => 'boolean',
+    ];
+
+    // 3. Зв'язок: Одне завдання належить одному користувачу
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
