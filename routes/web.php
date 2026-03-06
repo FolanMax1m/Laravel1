427da6c36a0dda475c5f64ea638d1214ca31be9e
Danyil <34955388-QuadrantSkillful@users.noreply.gitlab.com>
2026-03-06 15:25:32 +0000

    Update file web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,6 +17,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/setup-database-secret-url', function () {
+    try {
+        Artisan::call('migrate', ['--force' => true]);
+        return 'Міграції успішно виконані: <br><pre>' . Artisan::output() . '</pre>';
+    } catch (\Exception $e) {
+        return 'Помилка: ' . $e->getMessage();
+    }
 });
 
 require __DIR__.'/auth.php';
