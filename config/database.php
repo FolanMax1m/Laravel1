6e2a6fc7c5ec0268ed599723da39124cb19c9dd7
Danyil <34955388-QuadrantSkillful@users.noreply.gitlab.com>
2026-03-06 15:46:55 +0000

    Update file database.php

--- config/database.php
+++ config/database.php
@@ -96,9 +96,6 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
-            'options' => [
-                PDO::PGSQL_ATTR_OPTIONS => "endpoint=ep-long-sunset-a9pc3daq-pooler"
-            ],
         ],
 
         'sqlsrv' => [
